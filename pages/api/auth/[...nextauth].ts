a9b30710c3b88617bea325c555581cbeab14347b
Lola <lolivet@jolicode.com>
2022-03-28 15:08:17 +0200

    Add nextauth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,13 +6,12 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default NextAuth({
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 })
